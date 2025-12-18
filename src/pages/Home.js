cabebba6cf8f8a42cd3fdaf4c904946bb1ae67bd
Alexander Novitsky <novitsky.413x@gmail.com>
2025-12-18 17:56:07 +0300

    353. Navigating Programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage() {
+    const navigate = useNavigate();
+
+    function navigateHandler() {
+        // navigate to /products programmatically
+        // in case if some action needs to be done before navigation
+        // or if some timer expired and you can call navigate() then
+        navigate('/products');
+    }
+
     return (
         <>
             <h1>My Home Page</h1>
             <p>
                 Go to the <Link to="/products">the list of products</Link>.
                 {/* Go to <a href="/products">the list of products</a>. */}
+            </p>
+            <p>
+                <button onClick={navigateHandler}>Navigate to Products</button>
             </p>
         </>
     );
