4c29bec674486a8d8073771dc651003d7467a715
Alexander Novitsky <novitsky.413x@gmail.com>
2025-12-18 14:48:31 +0300

    349. Navigating between Pages with Links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function HomePage() {
-    return <h1>My Home Page</h1>;
+    return (
+        <>
+            <h1>My Home Page</h1>
+            <p>
+                Go to the <Link to="/products">the list of products</Link>.
+                {/* Go to <a href="/products">the list of products</a>. */}
+            </p>
+        </>
+    );
 }
 export default HomePage;
